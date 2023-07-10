b839564c849ccbcf1e1e96588eb47e846cd9d817
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-07-10 14:42:37 -0400

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Lori Rothermel on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
